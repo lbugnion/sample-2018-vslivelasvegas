12282459b9db2dd7868a3f96f03f14276a780424
lbugnion <lbugnion@microsoft.com>
2018-03-03 12:53:47 -0800

    Added CoreWithMvvmLight

---
+++ 02 DotNetStandard/CoreWithMvvmLight/CoreWithMvvmLight/Model/DataService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace CoreWithMvvmLight.Model
+{
+    public class DataService : IDataService
+    {
+        private const string Url = "URL HERE";
+
+        public async Task<string> GetResult(int num1, int num2)
+        {
+            var client = new HttpClient();
+            var result = await client.GetStringAsync(
+                Url
+                    .Replace("{num1}", num1.ToString())
+                    .Replace("{num2}", num2.ToString()));
+
+            return result;
+        }
+    }
+}
