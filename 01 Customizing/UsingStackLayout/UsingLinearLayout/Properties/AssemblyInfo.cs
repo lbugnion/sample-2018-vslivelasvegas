4bd186a8ddd63f9a335b36180edd3318b4372b1e
lbugnion <lbugnion@microsoft.com>
2018-03-10 16:28:40 -0500

    Renamed UsingStackLayout into UsingLinearLayout
    Some clean up, some snippets

--- 01 Customizing/UsingStackLayout/UsingLinearLayout/Properties/AssemblyInfo.cs
+++ 01 Customizing/UsingStackLayout/UsingLinearLayout/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using Android.App;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("UsingStackLayout")]
+[assembly: AssemblyTitle("UsingLinearLayout")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("UsingStackLayout")]
+[assembly: AssemblyProduct("UsingLinearLayout")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- 01 Customizing/UsingStackLayout/UsingLinearLayout/Resources/Resource.Designer.cs
+++ 01 Customizing/UsingStackLayout/UsingLinearLayout/Resources/Resource.Designer.cs
@@ -9,9 +9,9 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: global::Android.Runtime.ResourceDesignerAttribute("UsingStackLayout.Resource", IsApplication=true)]
+[assembly: global::Android.Runtime.ResourceDesignerAttribute("UsingLinearLayout.Resource", IsApplication=true)]
 
-namespace UsingStackLayout
+namespace UsingLinearLayout
 {
 	
 	
